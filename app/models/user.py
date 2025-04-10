138fd10eb5f0978c5e1dbcf5e5923fd691fb35cd
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-11 02:37:48 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,12 @@
-# app/models/user.py
-
 from pydantic import BaseModel, Field
 from typing import Optional
 
 class UserCreate(BaseModel):
     telegramId: str
-    first_name: Optional[str]
-    last_name: Optional[str]
-    username: Optional[str]
-    photo_url: Optional[str]
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    username: Optional[str] = None
+    photo_url: Optional[str] = None
 
 class UserInDB(UserCreate):
     id: Optional[str] = Field(alias="_id")
