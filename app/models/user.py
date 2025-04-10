6f5ed6d4af046be4976cc675a8601c383b8f97cf
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-11 04:20:26 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -2,11 +2,14 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class UserCreate(BaseModel):
-    telegramId: str
+    telegram_id: str = Field(..., alias="telegramId")
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     username: Optional[str] = None
     photo_url: Optional[str] = None
 
+    class Config:
+        allow_population_by_field_name = True  # Allows using 'telegram_id' or 'telegramId'
+
 class UserInDB(UserCreate):
-    id: Optional[str] = Field(alias="_id")
+    id: Optional[str] = Field(default=None, alias="_id")
