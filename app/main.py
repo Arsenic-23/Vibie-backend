9da22e3ff60ad07ee7f161af5f853a8eabd82ef2
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-11 02:31:19 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Vibie Backend")
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Vibie backend!"}
 from app.routes import auth, user, stream
 from app.database import connect_to_mongo
-
+from app.routes import websocket
+app.include_router(websocket.router, tags=["websocket"])
 app = FastAPI(title="Vibie Backend")
 
 @app.on_event("startup")
