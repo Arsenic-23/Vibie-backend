6636c28a72e4943e7c7687f58886fb3eae69d293
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 17:46:47 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,22 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MongoDB URI and Database name
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://romanrule7668:2tNPrgHMf9xCM82a@vibie.mmq1pj4.mongodb.net/vibie?retryWrites=true&w=majority&appName=Vibie")
+MONGO_URI = os.getenv("MONGO_U")
 DB_NAME = os.getenv("DB_NAME", "vibie_db")
 
 # API keys or any other sensitive information
-API_KEY = os.getenv("API_KEY", "AIzaSyB4k_EuVJ3JLFU-ywJWExJKflgcEKy1omQ")
-SEARCH_API = os.getenv("SEARCH_API", "AIzaSyDKZwdnrvfGJTiHdDGdemmjJ--jXnjwR1g")
-HITS_API = os.getenv("HITS_API", "https://your-music-api.com/hits")
+API_KEY = os.getenv("API_KEY")
+SEARCH_API = os.getenv("SEARCH_API")
+HITS_API = os.getenv("HITS_API")
 
 # WebSocket configurations
-WEBSOCKET_PORT = os.getenv("WEBSOCKET_PORT", 8001)
+WEBSOCKET_PORT = int(os.getenv("WEBSOCKET_PORT", 8001))  # Ensure the port is an integer
 WS_BROADCAST_URL = os.getenv("WS_BROADCAST_URL", "ws://localhost:8000/ws/stream/")
 
 # Authentication Config
-SECRET_KEY = os.getenv("SECRET_KEY", "1235679866_htrtjwwqeryoplknvxzdhj")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))  # Default to 30 minutes if not set
 
 # Logging configuration
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
