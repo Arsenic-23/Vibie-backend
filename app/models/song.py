59b98566a77b447169e89678005ad0381df8fa80
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 19:47:45 +0530

    Update song.py

--- app/models/song.py
+++ app/models/song.py
@@ -1,21 +1,11 @@
-from sqlalchemy import Column, Integer, String, Text
-from app.db.base_class import Base
+# app/models/song.py
 
-class Song(Base):
-    __tablename__ = "songs"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    artist = Column(String)
-    album = Column(String, nullable=True)
-    duration = Column(Integer)  # duration in seconds
-    url = Column(Text)
-    queued_for = relationship("SongQueue", back_populates="song")
-    streams = relationship("Stream", back_populates="current_song")
-    
-    def __init__(self, title: str, artist: str, album: str, duration: int, url: str):
-        self.title = title
-        self.artist = artist
-        self.album = album
-        self.duration = duration
-        self.url = url
+from pydantic import BaseModel
+from typing import Optional
+
+class Song(BaseModel):
+    title: str
+    artist: str
+    album: Optional[str] = None
+    duration: int  # duration in seconds
+    url: str
