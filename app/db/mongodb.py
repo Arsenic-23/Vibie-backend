491ed4b959baad4c7660a1498d10ed5d11bb142c
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 17:09:14 +0530

    Update mongodb.py

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,1 +1,21 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import ASCENDING
+from app.config import MONGO_URI
 
+client = None
+db = None
+
+async def connect_to_mongo():
+    global client, db
+    client = AsyncIOMotorClient(MONGO_URI)
+    db = client["vibie_db"]
+
+    # Ensure indexes (e.g. for stream_id to be unique)
+    await db.streams.create_index([("group_id", ASCENDING)], unique=True)
+    await db.users.create_index("user_id", unique=True)
+
+async def close_mongo_connection():
+    client.close()
+
+def get_db():
+    return db
