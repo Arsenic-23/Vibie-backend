0a03af35c376d12eef2776a1cd374fa098f0955f
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 17:11:46 +0530

    Update song.py

--- app/models/song.py
+++ app/models/song.py
@@ -1,1 +1,13 @@
+# app/models/song.py
 
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Song(BaseModel):
+    video_id: str
+    title: str
+    artist: Optional[str] = None
+    duration: int  # duration in seconds
+    thumbnail: Optional[str] = None
+    audio_url: Optional[str] = None  # Link to high-quality audio
