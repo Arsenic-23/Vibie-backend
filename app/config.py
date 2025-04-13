051db475b4109d401060087d7e884801fae35867
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-13 17:42:11 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 DB_NAME = os.getenv("DB_NAME", "vibie_db")
 
 # API keys or any other sensitive information
-API_KEY = os.getenv("API_KEY", "")
+API_KEY = os.getenv("API_KEY", "AIzaSyB4k_EuVJ3JLFU-ywJWExJKflgcEKy1omQ")
 SEARCH_API = os.getenv("SEARCH_API", "https://your-music-api.com/search")
 HITS_API = os.getenv("HITS_API", "https://your-music-api.com/hits")
 
@@ -18,7 +18,7 @@ WEBSOCKET_PORT = os.getenv("WEBSOCKET_PORT", 8001)
 WS_BROADCAST_URL = os.getenv("WS_BROADCAST_URL", "ws://localhost:8000/ws/stream/")
 
 # Authentication Config
-SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
+SECRET_KEY = os.getenv("SECRET_KEY", "1235679866_htrtjwwqeryoplknvxzdhj")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
