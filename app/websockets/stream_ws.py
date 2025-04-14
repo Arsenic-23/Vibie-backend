f284e9d16b9dddf3b1462e9acc4c8d1f1e4b3b36
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-14 06:16:45 +0530

    Update stream_ws.py

--- app/websockets/stream_ws.py
+++ app/websockets/stream_ws.py
@@ -1,7 +1,5 @@
-# app/websockets/stream_ws.py
-
 import json
-from fastapi import WebSocket, WebSocketDisconnect
+from fastapi import WebSocket, WebSocketDisconnect, WebSocketException
 from app.services.stream_service import StreamService
 
 class StreamWebSocket:
@@ -22,17 +20,18 @@ class StreamWebSocket:
         await websocket.send_json(stream_data)
 
     async def disconnect(self, websocket: WebSocket, stream_id: str):
-        self.active_connections[stream_id].remove(websocket)
+        if stream_id in self.active_connections:
+            if websocket in self.active_connections[stream_id]:
+                self.active_connections[stream_id].remove(websocket)
 
-        if len(self.active_connections[stream_id]) == 0:
-            del self.active_connections[stream_id]
+            if not self.active_connections[stream_id]:
+                del self.active_connections[stream_id]
 
     async def broadcast_stream_update(self, stream_id: str, data: dict):
         for connection in self.active_connections.get(stream_id, []):
             await connection.send_json(data)
 
     async def update_stream_state(self, stream_id: str, action: str):
-        # Update playback state, e.g., play, pause, next song, etc.
         action_data = {
             "action": action,
             "stream_id": stream_id
@@ -40,6 +39,27 @@ class StreamWebSocket:
         await self.broadcast_stream_update(stream_id, action_data)
 
     async def update_queue(self, stream_id: str):
-        # Send updated queue information to all connected users
         queue_data = self.stream_service.get_queue_data(stream_id)
         await self.broadcast_stream_update(stream_id, queue_data)
+
+
+# Define the actual WebSocket endpoint to be mounted
+stream_ws = StreamWebSocket()
+
+async def stream_ws_endpoint(websocket: WebSocket, stream_id: str):
+    try:
+        await stream_ws.connect(websocket, stream_id)
+
+        while True:
+            data = await websocket.receive_text()
+            message = json.loads(data)
+
+            if message.get("type") == "action":
+                await stream_ws.update_stream_state(stream_id, message.get("action"))
+            elif message.get("type") == "queue_update":
+                await stream_ws.update_queue(stream_id)
+
+    except WebSocketDisconnect:
+        await stream_ws.disconnect(websocket, stream_id)
+    except WebSocketException:
+        await websocket.close()
