e50fa6cced3f13ef909c632802dab5419ba560ba
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-14 05:55:46 +0530

    Create jwt_token.py

---
+++ app/utils/jwt_token.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from app.config import Config
+
+def generate_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=Config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, Config.SECRET_KEY, algorithm=Config.ALGORITHM)
+    return encoded_jwt
