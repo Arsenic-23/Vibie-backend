ef9ebeb8b341ffcc4b65b3a9a42e0fd738fe7ef3
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-18 22:30:03 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,44 +1,41 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from pymongo.errors import DuplicateKeyError
 from pymongo import ASCENDING
 from app.config import Config
 
-client = None
-db = None
+client: AsyncIOMotorClient = None
+db: AsyncIOMotorDatabase = None
 
 async def connect_to_mongo():
     """Connects to MongoDB and ensures necessary indexes are created."""
     global client, db
     try:
-        # Establish connection to MongoDB
         client = AsyncIOMotorClient(Config.MONGO_URI)
         db = client[Config.DB_NAME]
 
-        # Ensure necessary indexes are created
+        # Ensure indexes
         await db.users.create_index([("telegram_id", ASCENDING)], unique=True)
         await db.streams.create_index([("group_id", ASCENDING)], unique=True, sparse=True)
 
         print("Successfully connected to MongoDB and ensured indexes.")
-    
+
     except DuplicateKeyError as e:
-        # Handle error if the index already exists or if there are issues with index creation
         print(f"Error creating index: {e}")
-    
     except Exception as e:
-        # Handle other connection errors
         print(f"Error connecting to MongoDB: {e}")
         raise RuntimeError("Failed to connect to MongoDB")
 
 async def close_mongo_connection():
     """Closes the MongoDB connection."""
+    global client
     if client:
         client.close()
         print("MongoDB connection closed.")
     else:
         print("MongoDB connection is not established.")
 
-def get_db():
-    """Returns the database object."""
-    if not db:
+def get_db() -> AsyncIOMotorDatabase:
+    """Returns the MongoDB database instance."""
+    if db is None:
         raise RuntimeError("Database connection is not established.")
     return db
