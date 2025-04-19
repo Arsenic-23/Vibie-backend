5687e396d938f6897673fd4e8c8e6f76db579f78
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-19 05:43:57 +0530

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,7 +1,25 @@
+
 from pydantic import BaseModel
+from typing import Optional
+
 
 class UserCreate(BaseModel):
     telegram_id: int
-    name: str
-    username: str
-    photo_url: str = ""
+    name: Optional[str] = None
+    username: Optional[str] = None
+    photo_url: Optional[str] = None
+
+
+class UserResponse(BaseModel):
+    telegram_id: int
+    name: Optional[str] = None
+    username: Optional[str] = None
+    photo_url: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class LoginResponse(BaseModel):
+    access_token: str
+    user: UserResponse
