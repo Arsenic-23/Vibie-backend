f7f7746551c7b9eb2be3eff0d761ea08168894f6
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-22 17:10:11 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 
 from app.api import stream_routes, search_routes, user_routes
-from app.routes.register import router as register_router
 from app.websockets.stream_ws import stream_ws_endpoint
 from app.db.mongodb import connect_to_mongo, close_mongo_connection
 
@@ -35,7 +34,6 @@ app.add_middleware(
 app.include_router(stream_routes.router, prefix="/api/stream", tags=["Stream"])
 app.include_router(search_routes.router, prefix="/api/search", tags=["Search"])
 app.include_router(user_routes.router, prefix="/api/user", tags=["User"])
-app.include_router(register_router, prefix="/api/user", tags=["Register"])
 
 # WebSocket
 app.websocket("/ws/stream/{stream_id}")(stream_ws_endpoint)
