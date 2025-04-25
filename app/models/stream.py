9bbff3aaab2442425c3bd89b2077f05b5f9a8ddb
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-26 01:35:07 +0530

    Update stream.py

--- app/models/stream.py
+++ app/models/stream.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel, Field
-from app.models.user import User
 from app.models.song import Song
 
 class Stream(BaseModel):
